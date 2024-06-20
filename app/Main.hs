4bdc946b484adf30f90c7c4a022bab660501b7ae
Oleh Marakhovskyi <oleg.marakhovsky@gmail.com>
2024-06-20 18:37:21 +0300

    Cleanup + added alphabet

--- app/Main.hs
+++ app/Main.hs
@@ -6,36 +6,6 @@ import System.Environment( getArgs )
 import Codec.Picture
 import Codec.Picture.Types
 
-{-
-  We want an output to be 70x20
-  or maybe having 2 chars per convolution so that it will be more sizable
-
-  Convolution must be small, like <4 probably?
-  Find a set of convolution operations for a given image width and height
-
-  Would love to use cutting code for convolution and asciifying from pigworker/CS410-17
-  Is there a similar data structure in stdlib?
-  Maybe it's something used in xmonad?
-  xmonad seems to have similar structure, but it has a lot of overhead having to respond
-  to user commands, which we don't have to do yet
--}
-
- --   _ _ _ _ _
- -- | 4       4
- -- |   4   4
- -- |     4
- -- |   4   4
- -- | 4       4
-
-instance ColorSpaceConvertible PixelRGB8 Pixel8 where
-  {-# INLINE convertPixel #-}
-  -- TODO: use better algorithm (see color lecture)
-  -- convertPixel (PixelRGB8 r g b) = fromIntegral (floor (0.33 * (fromIntegral ((fromIntegral r) + (fromIntegral g) + (fromIntegral b)))))
-  convertPixel (PixelRGB8 r g b) = max r (max g b)
-
-convertP8 :: DynamicImage -> Image Pixel8
-convertP8 = convertImage . convertRGB8
-
 convertRGBF :: DynamicImage -> Image PixelRGBF
 convertRGBF = promoteImage . convertRGB8
 
@@ -98,19 +68,19 @@ preAsciify img = map (\ y -> map (`bwify` y) [0..w-1]) [0..h-1]
 
 
 asciify :: Image PixelF -> [String]
-asciify img = map (map charAt) (preAsciify img)
+asciify img = map (concatMap charAt) (preAsciify img)
   where
-    charAt :: [BW] -> Char
-    charAt x | length (filter (== B) x) == 1 = '.'
-             | length (filter (== B) x) == 2 = ':'
-             | length (filter (== B) x) == 3 = 'o'
-             | length (filter (== B) x) == 4 = 'O'
-             | length (filter (== B) x) == 5 = '8'
-             | length (filter (== B) x) == 6 = 'M'
-             | length (filter (== B) x) == 7 = 'W'
-             | length (filter (== B) x) == 8 = 'M'
-             | length (filter (== B) x) == 8 = 'M'
-             | otherwise = ' '
+    charAt :: [BW] -> String
+    charAt x | length (filter (== B) x) == 1 = ".  "
+             | length (filter (== B) x) == 2 = ":  "
+             | length (filter (== B) x) == 3 = "o  "
+             | length (filter (== B) x) == 4 = "O  "
+             | length (filter (== B) x) == 5 = "8  "
+             | length (filter (== B) x) == 6 = "M  "
+             | length (filter (== B) x) == 7 = "W  "
+             | length (filter (== B) x) == 8 = "M  "
+             | length (filter (== B) x) == 8 = "M  "
+             | otherwise = "   "
 
 edgeConv = [[  0.0, -1.0,  0.0],
             [ -1.0,  4.0, -1.0],
